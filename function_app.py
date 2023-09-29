78461e0ed280f3158688bfb9f6ac5c899f8b3031
Felix Lindgren <felix.p.lindgren@gmail.com>
2023-09-29 18:44:19 +0000

    change cron

--- function_app.py
+++ function_app.py
@@ -4,7 +4,7 @@ import main
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 0 0/2 * * *", arg_name="myTimer", run_on_startup=True,
+@app.schedule(schedule="0 0 0 * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
