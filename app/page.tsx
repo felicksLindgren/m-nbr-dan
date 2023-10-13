21a04d24fb7371ab5f9fbd56a3a4577358bdf310
Felix Lindgren <felix.p.lindgren@gmail.com>
2023-10-13 23:51:51 +0200

    just the basics

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle"
 import Image from "next/image"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ModeToggle />
+      <Image src="https://www.moonboard.com/content/images/holds/h59.png" alt="h59" width={100} height={100} />
     </main>
   )
 }
