88bc54402e14a38f044ba336bb7aecbe0fbb5229
Felix Lindgren <felix.p.lindgren@gmail.com>
2023-10-21 10:28:37 +0000

    infinite scroll on feed

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,32 +1,46 @@
-import { ModeToggle } from "@/components/mode-toggle"
+import InfiniteScroll from "@/components/infinite-scroll"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
 import cosmos from "@/lib/cosmos"
-import Link from "next/link"
+import { ChevronRightCircleIcon, MoreHorizontalIcon } from "lucide-react"
 
-const QUERY = "SELECT TOP 2 * FROM c";
+type PageProps = {
+  searchParams: URLSearchParams
+}
 
-export default async function Feed() {
+export default async function Feed({ searchParams }: PageProps) {
+  const size = searchParams["size"] || 5;
+  const QUERY = `SELECT TOP ${size} * FROM c ORDER BY c.repeats DESC`;
   const { resources: problems } = await cosmos.container.items.query<Problem>(QUERY).fetchAll();
 
   return (
     <main className="flex min-h-screen flex-col items-start">
-      {problems.map((problem) => (
-        <Card key={problem.id} className="w-[100%]">
-          <CardHeader>
-            <CardTitle>{problem.name}</CardTitle>
-            <CardDescription>{problem.grade} set by {problem.setby} with {problem.repeats} repeats</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <ModeToggle />
-          </CardContent>
-          <CardFooter>
-            <Link href="/feed/create">
-              <Button>Log Ascent</Button>
-            </Link>
-          </CardFooter>
-        </Card>
-      ))}
+      <InfiniteScroll>
+        {problems.map((problem) => (
+          <Sheet key={problem.id}>
+            <Card className="w-[100%]">
+              <CardHeader>
+                <CardTitle>{problem.name}</CardTitle>
+                <CardDescription>{problem.grade} set by {problem.setby} with {problem.repeats} repeats</CardDescription>
+              </CardHeader>
+              <CardContent className="flex justify-end">
+                  <SheetTrigger asChild>
+                    <Button variant="ghost" size="icon">
+                      <MoreHorizontalIcon />
+                    </Button>
+                  </SheetTrigger>
+              </CardContent>
+            </Card>
+            <SheetContent className="w-[80%] sm:w-[80%]">
+              <SheetHeader className="pb-3">
+                <SheetTitle>{problem.name}</SheetTitle>
+                <SheetDescription>{problem.grade} set by {problem.setby} with {problem.repeats} repeats</SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        ))}
+      </InfiniteScroll>
     </main>
   )
 }
---
+++ components/infinite-scroll.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
+
+type InfiniteScrollProps = {
+    increment?: number;
+    children: React.ReactNode[];
+    skeleton?: React.ReactNode;
+};
+
+export default function InfiniteScroll({ children, increment = 5 }: InfiniteScrollProps) {
+    const ref = useRef<HTMLDivElement>(null);
+    const { push } = useRouter();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                if (entries[0].isIntersecting) {
+                    push(`?size=${children.length + increment}`, { scroll: false });
+                }
+            },
+            { rootMargin: "228px" }
+        );
+
+        if (ref.current) {
+            observer.observe(ref.current);
+        }
+
+        return () => {
+            if (ref.current) {
+                observer.unobserve(ref.current);
+            }
+        };
+    }, [ref, children]);
+
+    return (
+        <>
+            {children}
+            <div ref={ref} />
+        </>
+    );
+}
