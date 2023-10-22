03f7c7f3a8831cba1892edd79f45087c5780d48b
Felix Lindgren <felix.p.lindgren@gmail.com>
2023-10-22 18:34:52 +0000

    changing size of holds

--- components/board.tsx
+++ components/board.tsx
@@ -8,20 +8,14 @@ type BoardProps = {
 const COLUMNS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
 const ROWS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
-export default function Board({ moves }: BoardProps) {
-    console.log(moves);
+export default function Board({ }: BoardProps) {
     return (
         <div className="grid gap-1 bg-muted">
             {ROWS.map((row) => (
                 <div key={row} className="flex justify-evenly items-center w-[100%]">
                     {COLUMNS.map((column) => (
-                        <div key={column} className="w-[45px] h-[45px]">
-                            <Image 
-                                src="https://moonboard.com/content/images/holds/h91.png"
-                                height={45}
-                                width={45}
-                                alt="Hold"
-                            />
+                        <div key={column} className="w-6 h-6">
+                            <Image src="https://moonboard.com/content/images/holds/h91.png" width="24" height="24" alt="Hold" />
                         </div>
                     ))}
                 </div>
