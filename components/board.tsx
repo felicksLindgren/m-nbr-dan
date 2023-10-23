224e84104f22b50e7ffcdc0df4713a2e41154e55
Felix Lindgren <felix.p.lindgren@gmail.com>
2023-10-23 15:29:34 +0000

    nu börjar vi snacka

--- components/board.tsx
+++ components/board.tsx
@@ -1,4 +1,4 @@
-import { Problem } from "@/types"
+import { Move, Problem } from "@/types"
 import Image from "next/image";
 
 type BoardProps = {
@@ -8,16 +8,19 @@ type BoardProps = {
 const COLUMNS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
 const ROWS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
-export default function Board({ }: BoardProps) {
+export default function Board({ moves }: BoardProps) {
+
+
     return (
         <div className="grid gap-1 bg-muted">
-            {ROWS.map((row) => (
-                <div key={row} className="flex justify-evenly items-center w-[100%]">
-                    {COLUMNS.map((column) => (
-                        <div key={column} className="w-6 h-6">
-                            <Image src="https://moonboard.com/content/images/holds/h91.png" width="24" height="24" alt="Hold" />
-                        </div>
-                    ))}
+            {ROWS.reverse().map((row) => (
+                <div key={row} className="flex justify-center items-center">
+                    {COLUMNS.map((column) => {
+                        const selected = moves.find((move) => move.description === `${column}${row}`);
+                        return (
+                            <Image className={selected ? "border-2 rounded-full border-primary" : ""} key={column} src="https://moonboard.com/content/images/holds/h91.png" width="30" height="30" alt="Hold" />
+                        )
+                    })}
                 </div>
             ))}
         </div>
